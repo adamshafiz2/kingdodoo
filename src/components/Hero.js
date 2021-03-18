7e8b8cff4858149394e007526b1e22253f785d08
adamshafiz2 <adamshafiz2@gmail.com>
2021-03-18 14:47:03 +0000

    final present

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,11 @@
-import ButtonContainer from "./ButtonContainer"
+
 function Hero() {
-    const styles = {
-        background: "red",
-        height: 40,
-        width: 150,
-        borderRadius: 75,
-        color: "white"
-    }
+    
     return (
         <header>
             <div className="hero">
                 <h2>KING DODOO FITNESS</h2>
                 <h4>I PROVIDE AN ENERGETIC BODY TRANSFORMATION </h4>
-                {/* <ButtonContainer styles={styles} title="Book Now"/> */}
             </div>
         </header>
     )
