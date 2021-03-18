d1b0f61a60a10adc76012cdbc7ce3bd2dd758d9f
adamshafiz2 <adamshafiz2@gmail.com>
2021-03-18 14:37:41 +0000

    third commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 function Counter() {
     const [count, setCount]=useState(0)
     return(
-        <div>
+        <div className="clients">
             <button onClick={() => setCount(count + 1)}>BOOK NOW</button>
             <div>NUMBER OF CLIENTS:{count} </div>
             <button onClick={() => setCount(count - 1)}>SIGN OUT</button>
